e5e9ace1ee0814b134dbcc5ed96f308bfa7fbd58
nguyenvanhao <nguyenvanhao.ktpm02@gmail.com>
2022-06-18 09:43:20 +0700

    push project

---
+++ avada-todo/src/hooks/useFetchApi.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export default function useFetchApi({ url, datas }) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchData() {
+    try {
+     setLoading(true)
+      const res = await fetch(url);
+      const respData = await res.json();
+
+      setData(respData.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+        setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return {
+    data,
+    setData,
+    loading
+  };
+}
---
+++ avada-todo/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+
+import "@shopify/polaris/build/esm/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
